4184b081732e73b088a0ff79340e73c1da322c0e
ascazzieri <andrea.scazzieri@applied.it>
2022-10-04 14:30:24 +0200

    Configurator_04_10_22

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Header'
+import Navbar from '../elements/Navbar/Navbar'
+
+function Header() {
+    return (
+        <Navbar />
+    )
+}
+
+export default Header;
